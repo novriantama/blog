42d4e830b2d76fc3796263edb8ec89728492028c
novriantama <novriantamam@gmail.com>
2021-09-14 20:50:50 +0700

    change to async await

--- controllers/article.js
+++ controllers/article.js
@@ -4,34 +4,29 @@ const path = require("path");
 const Article = require("../models/article");
 const User = require("../models/user");
 
-exports.getArticles = (req, res, next) => {
+exports.getArticles = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 5;
-  let totalItems;
-  Article.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Article.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((articles) => {
-      res.status(200).json({
-        message: "Fetched articles successfully.",
-        posts: articles,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const totalItems = await Article.find()
+      .countDocuments();
+    const articles = await Article.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    res.status(200).json({
+      message: "Fetched articles successfully.",
+      posts: articles,
+      totalItems: totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.createArticle = (req, res, next) => {
+exports.createArticle = async (req, res, next) => {
   if (!req.file) {
     const error = new Error("No image provided.");
     error.statusCode = 422;
@@ -40,58 +35,52 @@ exports.createArticle = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-  let creator;
   const article = new Article({
     title: title,
     content: content,
     imageUrl: imageUrl,
     creator: req.userId,
   });
-  article
-    .save()
-    .then((result) => {
-      return User.findById(req.userId);
-    })
-    .then((user) => {
-      creator = user;
-      user.posts.push(article);
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({
-        message: "Article created successfully!",
-        article: article,
-        creator: { _id: creator._id, name: creator.name },
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next();
-    });
-};
 
-exports.getArticle = (req, res, next) => {
+  try {
+    await article.save();
+
+    const user = await User.findById(req.userId);
+    user.posts.push(article);
+    await user.save();
+
+    res.status(201).json({
+      message: "Article created successfully!",
+      article: article,
+      creator: { _id: user._id, name: user.name },
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next();
+  }
+}
+
+exports.getArticle = async (req, res, next) => {
   const articleId = req.params.articleId;
-  Article.findById(articleId)
-    .then((article) => {
-      if (!article) {
-        const error = new Error("Could not find article.");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ message: "Article fetched.", article: article });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const article = await Article.findById(articleId);
+    if (!article) {
+      const error = new Error("Could not find article.");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: "Article fetched.", article: article });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); 
+  }
 };
 
-exports.updateArticle = (req, res, next) => {
+exports.updateArticle = async (req, res, next) => {
   const articleId = req.params.articleId;
   const title = req.body.title;
   const content = req.body.content;
@@ -104,70 +93,65 @@ exports.updateArticle = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  Article.findById(articleId)
-    .then((article) => {
-      if (!article) {
-        const error = new Error("Could not find article.");
-        error.statusCode = 404;
-        throw error;
-      }
-      if (article.creator.toString() !== req.userId) {
-        const error = new Error("Not authorized!");
-        error.statusCode = 403;
-        throw error;
-      }
-      if (imageUrl !== article.imageUrl) {
-        clearImage(article.imageUrl);
-      }
-      article.title = title;
-      article.imageUrl = imageUrl;
-      article.content = content;
-      return article.save();
-    })
-    .then((result) => {
-      res.status(200).json({ message: "Article updated!", article: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+
+  try {
+    const article = await Article.findById(articleId);
+    if (!article) {
+      const error = new Error("Could not find article.");
+      error.statusCode = 403;
+      throw error;
+    }
+    if (article.creator.toString() !== req.userId) {
+      const error = new Error("Not authorized.");
+      error.statusCode = 403;
+      throw error;
+    }
+    if (imageUrl !== article.imageUrl) {
+      clearImage(article.imageUrl);
+    }
+    article.title = title;
+    article.imageUrl = imageUrl;
+    article.content = content;
+    await article.save();
+
+    res.status(200).json({ message: "Article updated!", article: article });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);    
+  }
 };
 
-exports.deleteArticle = (req, res, next) => {
+exports.deleteArticle = async (req, res, next) => {
   const articleId = req.params.articleId;
-  Article.findById(articleId)
-    .then((article) => {
-      if (!article) {
-        const error = new Error("Could not find article.");
-        error.statusCode = 404;
-        throw error;
-      }
-      if (article.creator.toString() !== req.userId) {
-        const error = new Error("Not authorized!");
-        error.statusCode = 403;
-        throw error;
-      }
-      clearImage(article.imageUrl);
-      return Article.findByIdAndRemove(articleId);
-    })
-    .then((result) => {
-      return User.findById(req.userId);
-    })
-    .then((user) => {
-      user.posts.pull(articleId);
-      return user.save();
-    })
-    .then((result) => {
-      res.status(200).json({ message: "Deleted article." });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+
+  try {
+    const article = await Article.findById(articleId);
+    if (!article) {
+      const error = new Error("Could not find article.");
+      error.statusCode = 404;
+      throw error;
+    }
+    if (article.creator.toString() !== req.userId) {
+      const error = new Error("Not authorized!");
+      error.statusCode = 403;
+      throw error;
+    }
+    clearImage(article.imageUrl);
+    await Article.findByIdAndRemove(articleId);
+
+    const user = await User.findById(req.userId);
+    user.posts.pull(articleId)
+    await user.save();
+
+    res.status(200).json({ message: "Deleted article." });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 const clearImage = (filePath) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,68 +3,62 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
-  bcrypt
-    .genSalt(10)
-    .then((salt) => {
-      return bcrypt.hash(password, salt);
-    })
-    .then((hashedPw) => {
-      const user = new User({
-        email: email,
-        password: hashedPw,
-        name: name,
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "User created!", userId: result._id });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPw = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name,
     });
+    await user.save();
+
+    res.status(201).json({ message: "User created!", userId: user._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);    
+  }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   let userData;
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        const error = new Error("Email not found");
-        error.statusCode = 401;
-        throw error;
-      }
-      userData = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isPassword) => {
-      if (!isPassword) {
-        const error = new Error("Wrong password");
-        error.statusCode = 401;
-        throw error;
-      }
-      const token = jwt.sign(
-        {
-          email: userData.email,
-          userId: userData._id.toString(),
-        },
-        "passwordrahasia",
-        { expiresIn: "7d" }
-      );
-      res.status(200).json({ token: token, userId: userData._id.toString() });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+
+  try {
+    const user = await User.findOne({email: email});
+    if (!user) {
+      const error = new Error("Email not found");
+      error.statusCode = 401;
+      throw error;
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      const error = new Error("Wrong password");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: userData.email,
+        userId: userData._id.toString(),
+      },
+      "passwordrahasia",
+      { expiresIn: "7d" }
+    );
+    res.status(200).json({ token: token, userId: userData._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
